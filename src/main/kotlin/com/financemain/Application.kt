4cb08e1c6ae1e6fdac64aa68bf57c3d1dec67db7
WesleyAugusto <WesleyAugusto>
2022-11-09 22:16:21 +0000

    Initial commit

---
+++ src/main/kotlin/com/financemain/Application.kt
@@ -0,0 +1,8 @@
+package com.financemain
+
+import io.micronaut.runtime.Micronaut.*
+
+fun main(args: Array<String>) {
+	run(*args)
+}
+
---
+++ src/test/kotlin/com/financemain/FinanceMainTest.kt
@@ -0,0 +1,12 @@
+package com.financemain
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.kotest.annotation.MicronautTest
+import io.kotest.core.spec.style.StringSpec
+
+@MicronautTest
+class FinanceMainTest(private val application: EmbeddedApplication<*>): StringSpec({
+
+    "test the server is running" {
+        assert(application.isRunning)
+    }
+})
